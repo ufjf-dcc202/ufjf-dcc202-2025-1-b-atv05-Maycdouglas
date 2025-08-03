f44cd9dba6325854bfe57de896aab8a3cff9ff3b
Maycon Douglas <65413165+Maycdouglas@users.noreply.github.com>
2025-08-03 17:52:27 -0300

    Insere funções de disco

---
+++ main.js
@@ -0,0 +1,38 @@
+import { getTabuleiro, selecionaDisco } from "./discos.js";
+
+const eH1 = document.querySelector("h1");
+eH1.textContent = "Jogo de Discos";
+
+const eTabuleiro = criaTabuleiro();
+atualizaTabuleiro();
+document.body.append(eTabuleiro);
+
+function criaTabuleiro() {
+  const eTabuleiro = document.createElement("div");
+  eTabuleiro.classList.add("tabuleiro");
+  return eTabuleiro;
+}
+
+function criaDisco(cor, posicao) {
+  const novoDisco = document.createElement("div");
+  novoDisco.classList.add("disco");
+  novoDisco.classList.add(cor);
+  novoDisco.dataset.posicao = posicao;
+  return novoDisco;
+}
+
+function cliqueDisco(evento) {
+  const posicao = Number(evento.target.dataset.posicao);
+  selecionaDisco(posicao);
+  atualizaTabuleiro;
+}
+
+function atualizaTabuleiro() {
+  eTabuleiro.innerHTML = "";
+  const tabuleiro = getTabuleiro();
+  for (let i = 0; i < tabuleiro.length; i++) {
+    const disco = criaDisco(tabuleiro[i], i);
+    eTabuleiro.append(disco);
+    disco.addEventListener("click", cliqueDisco);
+  }
+}
